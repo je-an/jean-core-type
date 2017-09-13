02620c760ec3c7f469309f898268204084c62913
je-an <mail@jeremiasandris.de>
2017-09-13 20:32:23 +0200

    Add NotImplementedError

--- src/error/InterfaceError.js
+++
@@ -1,8 +0,0 @@
-define([], function () {
-    var InterfaceError = function (message) {
-        this.name = "Interface Error";
-        this.message = (message || "Method must be implemented in Class");
-    };
-    InterfaceError.prototype = Error.prototype;
-    return InterfaceError; 
-}); 
